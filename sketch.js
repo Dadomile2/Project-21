f00983baa8599bbc237201ecceaee8455b0f4c48
Dadomile2 <94705965+Dadomile2@users.noreply.github.com>
2022-03-02 17:42:03 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music,musicSound;
+
+function preload(){
+    musicSound = loadSound("music.mp3")
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = "orange";
+    
+    block3 = createSprite(510,580,200,30);
+    block3.shapeColor = "red";
+    
+    block4 = createSprite(725,580,200,30);
+    block4.shapeColor = "green";
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+    ball.velocityX = 10;
+    ball.velocityY = 10;
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges = createEdgeSprites();
+    ball.bounceOff(edges);
+
+    
+    
+    if(block1.isTouching(ball)){
+        ball.shapeColor = "blue";
+        ball.bounceOff(block1);
+        musicSound.play();
+    }
+
+
+
+    if(block2.isTouching(ball)){
+        ball.shapeColor = "orange";
+        ball.bounceOff(block2);
+        musicSound.play();
+    }
+
+
+
+    if(block3.isTouching(ball)){
+        ball.shapeColor = "red";
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+        musicSound.stop();
+    }
+
+
+
+
+    if(block4.isTouching(ball)){
+        ball.shapeColor = "green";
+        ball.bounceOff(block4);
+        musicSound.play();
+    }
+
+    drawSprites();
+}
